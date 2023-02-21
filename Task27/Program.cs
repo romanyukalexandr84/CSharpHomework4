1e113d8fd9e75c68deeea9f6ddd92ed644c316b1
romanukalexandr <romanukalexandr@gmail.com>
2023-02-21 23:09:23 +0300

    add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿int SumDigit (string digit)
+{
+int leng = digit.Length;
+int num = int.Parse(digit);
+
+int ost = 0;
+int sum = 0;
+
+for (int i=0; i < leng; i++)
+{
+    ost = num % 10;
+    num = num / 10;
+    sum = sum + ost;
+}
+return sum;
+}
+
+Console.WriteLine ("Введите целое положительное число");
+string chislo = Console.ReadLine();
+Console.WriteLine ("Сумма цифр в числе = "+SumDigit (chislo));
